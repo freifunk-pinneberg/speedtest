311c17b42ca22a443a5ee267ddc6d968dabe98c4
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2019-11-01 21:41:23 +0100

    Aufräumen

--- backend/garbage.php
+++ backend/garbage.php
@@ -5,7 +5,7 @@
 @ini_set('output_handler', '');
 // Headers
 header('HTTP/1.1 200 OK');
-if(isset($_GET["cors"])){
+if (isset($_GET["cors"])) {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST');
 }
@@ -19,13 +19,16 @@ header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 // Generate data
-$data=openssl_random_pseudo_bytes(1048576);
+$data = openssl_random_pseudo_bytes(1048576);
 // Deliver chunks of 1048576 bytes
-$chunks=isset($_GET['ckSize']) ? intval($_GET['ckSize']) : 4;
-if(empty($chunks)){$chunks = 4;}
-if($chunks>1024){$chunks = 1024;}
-for($i=0;$i<$chunks;$i++){
+$chunks = isset($_GET['ckSize']) ? intval($_GET['ckSize']) : 4;
+if (empty($chunks)) {
+    $chunks = 4;
+}
+if ($chunks > 1024) {
+    $chunks = 1024;
+}
+for ($i = 0; $i < $chunks; $i++) {
     echo $data;
     flush();
 }
-?>
